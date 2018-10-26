0462caa3304dd1b50a91a1b8c3755368b856ea55
Ujwal Battar <ujwal.battar@gmail.com>
2018-10-25 23:21:34 -0700

    setup rails

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/healthy_pets.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "env"]
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
